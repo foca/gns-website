a0cdc2c833f5c79cae658a9d6407b0d974635522
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-06-28 16:56:05 -0300

    Sucky design is sucky, but it's better than nothing :P

--- gaming-night-society.rb
+++ gaming-night-society.rb
@@ -3,22 +3,3 @@ require "sinatra"
 get "/" do
   erb :home
 end
-
-__END__
-
-@@layout
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html lang="es">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>Gaming Night Society</title>
-  <link rel="stylesheet" type="text/css" href="/main.css">
-</head>
-<body>
-  <h1>Gaming Night Society</h1>
-  <%= yield %>
-</body>
-</html>
-
-@@home
-<h2>Próximamente</h2>
